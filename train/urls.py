93587a53d68b608d0619db7964ddf87618cfb87d
root <root@localhost.localdomain>
2017-10-24 13:57:37 +0200

    ajout structure routes

--- train/urls.py
+++ train/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from utils import user_is_student
+#from utils import user_is_student
 
 from . import views
+from . import viewsProf
 
 urlpatterns = [
-    url(r'^scenario/(?P<id>\d+)/$', views.scenario, name='student_dashboard'),
-    url(r'^data/', views.dashboard),
-    url(r'^create_scenario/$', views.scenario, name='student_dashboard'),
-    url(r'^edit_scenario/$', views.scenario, name='student_dashboard'),
-    url(r'^list_scenario/$', views.scenario, name='student_dashboard'),
+    url(r'^scenario/(?P<id>\d+)', viewsProf.home, name='student_dashboard'),
+    url(r'^data/', viewsProf.home),
+    url(r'^create_scenario', viewsProf.home, name='student_dashboard'),
+    url(r'^edit_scenario/', views.dashboard, name='student_dashboard'),
+    url(r'^list_scenario/', views.dashboard, name='student_dashboard'),
 ]
