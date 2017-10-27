ef2bb512be64ee3ca8b80c04161d3efc9f4f94f2
sigustin <simon.gustin@student.uclouvain.be>
2017-10-27 11:02:33 +0200

    Add example tests with pyhamcrest in graph/tests.py

--- graph/tests.py
+++ graph/tests.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# Put tests here
+from hamcrest import *
+import unittest
+
+class ExampleTest(unittest.TestCase):
+	def example_equals(self):
+		a = 1
+		b = 1
+		assert_that(a, equal_to(b))
+		assert_that(a, is_(b))  # same as the line before (syntactic sugar)
+
+if __name__ == '__main__':
+	unittest.main()
 
