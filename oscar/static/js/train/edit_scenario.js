dc30c2d841c343195760c92d69a80dd0da576216
Antoine <habrantoine95@gmail.com>
2017-11-02 17:36:16 +0000

    description vidéo

--- oscar/static/js/train/edit_scenario.js
+++ oscar/static/js/train/edit_scenario.js
@@ -101,7 +101,7 @@ class EditScenario {
     getElemInputBlockVideo(elemVideo){
         let title = elemVideo.childNodes[1].childNodes[3].value;
         let url = elemVideo.childNodes[1].childNodes[9].childNodes[7].value
-        let description = "description"
+        
         return {"type":"VidElem", "data":{"title": title, "url": url, "description" : description}}
     }
 
@@ -167,6 +167,7 @@ class EditScenario {
         newelem.innerHTML = this.videoBlockElem.innerHTML;
         newelem.getElementsByClassName('title_vid')[0].value = this.data["elements"][index]["data"]["title"]
         newelem.getElementsByClassName('url')[0].value = this.data["elements"][index]["data"]["url"]
+        newelem.getElementsByClassName('description')[0].value = this.data["elements"][index]["data"]["description"]
         loadVideo(newelem.getElementsByClassName('addVid')[0])
         this.anchor.appendChild(newelem);
     }
