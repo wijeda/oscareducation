7f13cc56ca5980ec31693264b5795143aba08d17
GillonB <bastien.gillon@student.uclouvain.be>
2017-11-08 14:16:22 +0100

    Forbid user to rate(star and question) own resource, add unique constraint to rating tab! only rate 1 resource

--- rating/models.py
+++ rating/models.py
@@ -23,9 +23,12 @@ class Star_rating(models.Model):
         self.star = stars
         self.resource = resource
         self.rated_by = user
-        self.date = datetime.date
+        self.rated_on = datetime.date
         self.save()
         return
+
+    class Meta:
+        unique_together = ('resource','rated_by')
 
 
 class Rating(models.Model):
@@ -44,46 +47,13 @@ class Rating(models.Model):
     rated_on = models.DateTimeField()
     """Date of rating"""
 
-    def add_rating(self,resource,user,stars):
-        """Add rating to a resource
-
-            :param Resource: current resource to rate
-            :param User: current user rating the resource
-            :param stars: number of stars the user add to resource
-
-        """
-        if isinstance(user, Professor):
-            resource.star_prof = resource.star_prof + stars
-            resource.nbr_prof = resource.nbr_prof + 1
-            if stars >= 4:
-                author = User.objects.get(id=resource.added_by)
-                author.inc()
-
-        elif isinstance(user, Student):
-            resource.star_student = resource.star_student + stars
-            resource.nbr_student = resource.nbr_student + 1
-        else:
-            pass
-
-        answer = Answer.objects.get(answer_statement=stars)
-        question = Question.objects.get(id=0)
-
-        self.resource = resource
-        self.question = question
-        self.answer = answer
-        self.user = user
-        self.rated_on = datetime.date
-
-        '''We should implement a inc() function in Resource that increments and saves'''
-        resource.save()
-        self.save()
-
     def number_votes_answer(self,resource,question,answer):
         """Get the number of votes for answer at a question from a resource"""
         r = Rating.objects.get(resource=resource,question=question,answer=answer)
         return r.entry_set.count()
 
-
+    class Meta:
+        unique_together = ('resource','question','rated_by')
 
 
 class Answer(models.Model):
--- resources/models.py
+++ resources/models.py
@@ -2,13 +2,11 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
-from rating.models import Star_rating
+from rating.models import Star_rating,Rating
 from django.utils import timezone
 from users.models import Professor,Student
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
+
 
 """resources models"""
 
@@ -26,12 +24,32 @@ class Resource(models.Model):
 
 
     def add_star(self, rate, user):
-        """Add stars to a resource"""
-        f = Star_rating.objects.create(resource=self,star=rate,rated_by=user,rated_on=timezone.now())
-        return f
+        """
+        Creates or modifies the star rating for a resource by a user
+
+        :param rate: float number of stars given
+        :param user: user who is giving the stars
+        :return: the star object or None is the user can not rate the resource because he uploaded it
+        """
+        if self.added_by == user:
+            return None
+        star = Star_rating.objects.filter(resource=self,rated_by=user)
+        if star.exists() and len(star) == 1:
+            star= star.first()
+            star.star = rate
+            star.save()
+        elif not star.exists() and len(star) == 0:
+            star = Star_rating.objects.create(resource=self,star=rate,rated_by=user,rated_on=timezone.now())
+        else:
+            #Error
+            return None
+        return star
 
     def average(self):
-        """Returns a tuple containing the average stars for student and professors of this resource"""
+        """
+
+        :return: A tuple containing the average for students and average for professors
+        """
         all = Star_rating.objects.filter(resource=self)
         prof_nb = 0
         student_nb = 0
@@ -55,15 +73,44 @@ class Resource(models.Model):
 
         if student_nb==0:
             if prof_nb==0:
-                return (-1,-1)
+                return (0,0)
             else:
-                return (-1, star_prof / prof_nb)
+                return (0, star_prof / prof_nb)
         else:
             if prof_nb==0:
-                return (star_student / student_nb,-1)
+                return (star_student / student_nb,0)
             else:
                 return (star_student / student_nb, star_prof / prof_nb)
 
+    def add_rating(self,question,answer,user):
+        """
+        Creates or Modifies a question/answer to a resource by a user
+
+        :param question: the question answers by user
+        :param answer: the answer to question by user
+        :param user: the user answering a question
+        :return: The rating object or None if the user may not rate the resource
+        """
+        if user == self.added_by:
+            return None
+        rating = Rating.objects.filter(resource=self,rated_by=user,question=question)
+        if rating.exists() and len(rating)==1:
+            rating = rating.first()
+            rating.question = question
+            rating.answer = answer
+            rating.save()
+        elif not rating.exists() and len(rating) == 0:
+            rating = Rating.objects.create(
+                resource=self,
+                question=question,
+                answer=answer,
+                rated_by=user,
+                rated_on=timezone.now()
+            )
+        else:
+            #Error
+            return None
+        return rating
 
 #khanAcademy video reference data parsed from source url
 
